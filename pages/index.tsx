1f79f6936a9e81c037623e9ba557108518af33a0
Evgenii Khramkov <ewgeniux@gmail.com>
2018-11-03 23:06:00 +0300

    basic now app

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <Link href="/contacts">
+      <p>
+        test
+      </p>
+    </Link>
+  </div>
+)
+
+export default Index
